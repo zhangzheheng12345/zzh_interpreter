f7412e77bea1b393c6064bf0941749e87596178a
zhangzheheng12345 <76390451+zhangzheheng12345@users.noreply.github.com>
2021-02-26 21:25:24 +0800

    Update runner.cpp

--- interpreter/runner.cpp
+++ interpreter/runner.cpp
@@ -183,50 +183,7 @@ void runner::obj_adding_static()
 		
 	}
 	vas.statics.add_var(varname,vartype);
-}
-
-void runner::obj_adding_global()
-{
-	string varname,str_var_type;
-	var_ts vartype;
-	int i = 7;
-	for (i; sen_to_run[i] != ':'; i++)//get the varname
-		varname += sen_to_run[i];
-	i++;
-	for (i; is_alpha(sen_to_run[i]) || sen_to_run[i] == '_'; i++)//get the type
-		str_var_type += sen_to_run[i];
-	if (!strcmp(str_var_type.c_str(), "int"))//type:str to enum
-	{
-		vartype = Int;
-	}
-	else if (!strcmp(str_var_type.c_str(), "float"))
-	{
-		vartype = Float;
-	}
-	else if (!strcmp(str_var_type.c_str(), "string"))
-	{
-		vartype = String;
-	}
-	else if (!strcmp(str_var_type.c_str(), "bool"))
-	{
-		vartype = Bool;
-	}
-	else if(!strcmp(str_var_type.c_str(),"func"))
-	{
-		vartype = func;
-	}
-	else if(!strcmp(str_var_type.c_str(),"class"))
-	{
-		vartype = Class;
-	}
-	else
-	{
-		//check classes
-		//or output error
-		
-	}
-	globals.add_var(varname,vartype);
-}
+}		
 
 void runner::obj_parce()
 {
@@ -268,6 +225,14 @@ void runner::first_letter_check()
 		if (!strcmp(key_word.c_str(), "var"))
 		{
 			obj_adding_var();
+		}
+		else if (!strcmp(key_word.c_str(),"const"))
+		{
+			obj_adding_const();
+		}
+		else id (!strcmp(key_word.c_str(),"static"))
+		{
+			obj_adding_static();
 		}
 	}
 	else
